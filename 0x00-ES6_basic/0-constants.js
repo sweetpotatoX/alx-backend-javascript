2cf6c42634286d9ce541903b769f2a35dec0f658
sweetpotatoX <abdobahaa53@gmail.com>
2024-04-26 21:15:16 +0300

    Merge branch 'main' of https://github.com/sweetpotatoX/alx-backend-javascript

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -9,8 +9,6 @@ export function getLast() {
 
 export function taskNext() {
     let combination = 'But sometimes let';
-    combination + getLast();
-    let combination = 'But sometimes let';
     combination += getLast();
 
     return combination;
